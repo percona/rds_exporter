71ceecee39072f89d95bd451318a4cca7a3d8037
Andrii Skomorokhov <andrii.skomorokhov@percona.com>
2020-03-13 14:26:28 +0000

    PMM-4145 Extend test for disabled metrics.

--- basic/collector_test.go
+++ basic/collector_test.go
@@ -1,6 +1,7 @@
 package basic
 
 import (
+	"fmt"
 	"sort"
 	"strings"
 	"testing"
@@ -52,8 +53,13 @@ func TestCollectorDisableBasicMetrics(t *testing.T) {
 	cfg, err := config.Load("../config.tests.yml")
 	require.NoError(t, err)
 	client := client.New()
+	instanceGroups := make(map[bool][]string, 2)
 	for i := range cfg.Instances {
-		cfg.Instances[i].DisableBasicMetrics = true
+		// Disable basic metrics in odd instances.
+		isDisabled := i%2 == 1
+		cfg.Instances[i].DisableBasicMetrics = isDisabled
+		// Groups instance names by disabled or enabled metrics.
+		instanceGroups[isDisabled] = append(instanceGroups[isDisabled], cfg.Instances[i].Instance)
 	}
 	sess, err := sessions.New(cfg.Instances, client.HTTP(), false)
 	require.NoError(t, err)
@@ -62,15 +68,24 @@ func TestCollectorDisableBasicMetrics(t *testing.T) {
 
 	actualMetrics := helpers.ReadMetrics(helpers.CollectMetrics(c))
 	actualLines := helpers.Format(helpers.WriteMetrics(actualMetrics))
-	for _, line := range actualLines {
-		for _, inst := range cfg.Instances {
-			assert.Falsef(
-				t,
-				strings.Contains(inst.Instance, line),
-				"Instance %s with disabled metrics reseive: %s",
-				inst.Instance,
-				line,
-			)
+
+	// Check if all collected metrics do not contain metrics for instance whare disabled metrics.
+	hasMetricForInstance := func(lines []string, instanceName string) bool {
+		for _, line := range lines {
+			if strings.Contains(fmt.Sprintf("instance=%q", instanceName), line) {
+				return true
+			}
 		}
+		return false
+	}
+
+	// Scans if metrics contain a metric for the disabled instance (DisableBasicMetrics = true).
+	for _, inst := range instanceGroups[true] {
+		assert.Falsef(t, hasMetricForInstance(actualLines, inst), "Found metrics for disabled instance %s", inst)
+	}
+
+	// Scans if metrics contain a metric for the enabled instance (DisableBasicMetrics = false).
+	for _, inst := range instanceGroups[false] {
+		assert.Truef(t, hasMetricForInstance(actualLines, inst), "Did not find metrics for enabled instance %s", inst)
 	}
 }
