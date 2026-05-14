cc9d55eac7170e0d10a8206ba3608062c0b2b9db
Alex Demidoff <a@demidoff.me>
2026-05-14 10:51:04 +0300

    PMM-7 Fix the tests after moving to a different environment

--- sessions/sessions_test.go
+++ sessions/sessions_test.go
@@ -36,60 +36,59 @@ func TestSession(t *testing.T) {
 	sessions, err := New(cfg.Instances, client.HTTP(), logger, false)
 	require.NoError(t, err)
 
-	am56s, am56i := sessions.GetConfig("us-east-1", "autotest-aurora-mysql-56")
-	p10s, p10i := sessions.GetConfig("us-east-1", "autotest-psql-10")
-	m57s, m57i := sessions.GetConfig("us-west-2", "autotest-mysql-57")
-	ap11s, ap11i := sessions.GetConfig("us-west-2", "autotest-aurora-psql-11")
-	ns, ni := sessions.GetConfig("us-west-2", "no-such-instance")
+	am57s, am57i := sessions.GetConfig("us-east-2", "pmm-qa-aurora3-mysql-instance-1")
+	p13s, p13i := sessions.GetConfig("us-east-2", "pmm-qa-aurora-postgres-13-5-instance-1")
+	m84s, m84i := sessions.GetConfig("us-east-2", "pmm-qa-rds-mysql-8-4")
+	ap16s, ap16i := sessions.GetConfig("us-east-2", "pmm-qa-pgsql-16")
+	ns, ni := sessions.GetConfig("us-east-2", "no-such-instance")
 
-	if reflect.DeepEqual(am56s, p10s) {
-		assert.Fail(t, "autotest-aurora-mysql-56 and autotest-psql-10 should not share config - different keys (implicit and explicit)")
+	if reflect.DeepEqual(am57s, p13s) {
+		assert.Fail(t, "pmm-qa-aurora3-mysql-instance-1 and pmm-qa-aurora-postgres-13-5-instance-1 should not share config - different keys (implicit and explicit)")
 	}
-	if reflect.DeepEqual(p10s, m57s) {
-		assert.Fail(t, "autotest-psql-10 and autotest-mysql-57 should not share config - different regions")
+	if !reflect.DeepEqual(p13s, m84s) {
+		assert.Fail(t, "pmm-qa-aurora-postgres-13-5-instance-1 and pmm-qa-rds-mysql-8-4 should share config - same regions")
 	}
-	if !reflect.DeepEqual(m57s, ap11s) {
-		assert.Fail(t, "autotest-mysql-57 and autotest-aurora-psql-11 should share config")
+	if !reflect.DeepEqual(m84s, ap16s) {
+		assert.Fail(t, "pmm-qa-rds-mysql-8-4 and pmm-qa-pgsql-16 should share config")
 	}
 	if ns != nil {
 		assert.Fail(t, "no-such-instance does not exist")
 	}
 
-	am56iExpected := Instance{
-		Region:                     "us-east-1",
-		Instance:                   "autotest-aurora-mysql-56",
-		ResourceID:                 "db-OQT42DPIZWWQBVXQ2LH2BW3SV4",
+	am57iExpected := Instance{
+		Region:                     "us-east-2",
+		Instance:                   "pmm-qa-aurora3-mysql-instance-1",
+		ResourceID:                 "db-DFQSTXQUYKPPAFNHGTJ27P5PTU",
 		EnhancedMonitoringInterval: time.Minute,
 	}
-	p10iExpected := Instance{
-		Region:                     "us-east-1",
-		Instance:                   "autotest-psql-10",
-		ResourceID:                 "db-PUZFCRUUHY365QFJLTOUWRDOCQ",
+	p13iExpected := Instance{
+		Region:                     "us-east-2",
+		Instance:                   "pmm-qa-aurora-postgres-13-5-instance-1",
+		ResourceID:                 "db-ZUYAKAIMBFOPJOW3I5YKUI3XXY",
 		EnhancedMonitoringInterval: time.Minute,
 	}
-	m57iExpected := Instance{
-		Region:                     "us-west-2",
-		Instance:                   "autotest-mysql-57",
-		ResourceID:                 "db-QXZYJIL5GR3CBQ4XNCYU2AI5PE",
+	m84iExpected := Instance{
+		Region:                     "us-east-2",
+		Instance:                   "pmm-qa-rds-mysql-8-4",
+		ResourceID:                 "db-POWENXD5M5PQ34OZFZKSUPNX64",
 		EnhancedMonitoringInterval: time.Minute,
 	}
-	ap11iExpected := Instance{
-		Region:                     "us-west-2",
-		Instance:                   "autotest-aurora-psql-11",
-		ResourceID:                 "db-TYM5GWPPEMFCR5L6YX6ZBHUIUE",
+	ap16iExpected := Instance{
+		Region:                     "us-east-2",
+		Instance:                   "pmm-qa-pgsql-16",
+		ResourceID:                 "db-H5NM4CLYE5MF4DSEIZAX2BSWNE",
 		EnhancedMonitoringInterval: time.Minute,
 	}
 
-	assert.Equal(t, &am56iExpected, am56i)
-	assert.Equal(t, &p10iExpected, p10i)
-	assert.Equal(t, &m57iExpected, m57i)
-	assert.Equal(t, &ap11iExpected, ap11i)
+	assert.Equal(t, &am57iExpected, am57i)
+	assert.Equal(t, &p13iExpected, p13i)
+	assert.Equal(t, &m84iExpected, m84i)
+	assert.Equal(t, &ap16iExpected, ap16i)
 	assert.Nil(t, ni)
 
 	all := sessions.AllSessions()
 	assert.Equal(t, map[string][]Instance{
-		"us-east-1/" + os.Getenv("AWS_ACCESS_KEY"): {am56iExpected},
-		"us-east-1/": {p10iExpected},
-		"us-west-2/": {m57iExpected, ap11iExpected},
+		"us-east-2/" + os.Getenv("AWS_ACCESS_KEY"): {am57iExpected},
+		"us-east-2/": {p13iExpected, m84iExpected, ap16iExpected},
 	}, all)
 }
